6e117af0460f3bb23a3e6e0c05e1f2a47bd9ec34
javicond3 <4b3javi@gmail.com>
2020-02-27 14:44:14 +0100

    add eslint

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
 import '../assets/scss/main.scss';
 
-
-
 export default class App extends React.Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      helloWorld: "hola"
+      helloWorld: 'hola'
     };
   }
 
   componentDidMount() {
-      //code when component is mounted
-      this.setState({
-          helloWorld: "hola4"
-      })
+    //code when component is mounted
+    this.setState({
+      helloWorld: 'hola4'
+    })
   }
 
   render() {
     return (
       <div>
-          <h2>{this.state.helloWorld}</h2>
+        <h2>{this.state.helloWorld}</h2>
       </div>
 
     );
